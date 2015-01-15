8b6f3b20c878254d66cae525c937121a1ca6741d
Helen Hood <helen.c.hood@gmail.com>
2015-01-15 13:18:45 -0500

    Seed projects

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+projects_attrs = [
+  {
+    name: "Lock up world oil supply",
+    description: "Whaddya gonna do with your SUVs now, suckers!"
+  },
+  {
+    name: "Tank the U.S. dollar"
+  },
+  {
+    name: "Smite the world with plague of man-eating ladybugs",
+    description: "You won't think they're so cute when they're munching on you."
+  }
+]
+
+projects_attrs.each do |attrs|
+  project = Project.find_by(attrs)
+
+  if !project.nil?
+    puts "Project '#{attrs[:name]}' already in database."
+  else
+    Project.create(attrs)
+    puts "Project '#{attrs[:name]}' created."
+  end
+end
