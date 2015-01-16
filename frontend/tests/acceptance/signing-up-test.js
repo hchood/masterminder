b81de5d64b138edc63b62276cda8b6ab2c849f08
Helen Hood <helen.c.hood@gmail.com>
2015-01-16 12:58:11 -0500

    User can sign up (ember test passing)
    - Generate acceptance test:
    ember g acceptance-test signing-up
    - Add test for successful signup
    - Generate route & modify it:
    ember g route users/new
    - Add form to new template
    - Generate user model
    ember g model user email:string first_name:string last_name:string
    password:string bio:string
    - Generate user show route, modify route and template
    
    Deleted generated test files.
    
    Do I need password on the user model?

---
+++ frontend/tests/acceptance/signing-up-test.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+import startApp from '../helpers/start-app';
+import Pretender from 'pretender';
+
+var application, server;
+
+module('Acceptance: SigningUp', {
+  setup: function() {
+    application = startApp();
+
+    server = new Pretender();
+  },
+  teardown: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('user signs up', function() {
+  server.post('/api/v1/users', function(request) {
+    var userResponse = {
+      email: 'wizard@nefariousschemers.com',
+      first_name: 'Faizaan',
+      last_name: 'Shamsi',
+      bio: "That's just my face."
+    };
+
+    return [201, {"Content-Type": "application/json"}, JSON.stringify({user: userResponse})];
+  });
+
+  visit('/users/new');
+
+  fillIn('input[name="email"]', 'wizard@nefariousschemers.com');
+  fillIn('input[name="password"]', 'secretschemes');
+  fillIn('input[name="password_confirmation"]', 'secretschemes');
+  fillIn('input[name="first_name"]', 'Faizaan');
+  fillIn('input[name="last_name"]', 'Shamsi');
+  fillIn('textarea[name="bio"]', "That's just my face.");
+
+  click('input[type="submit"]');
+
+  andThen(function() {
+    equal(currentPath(), 'users.show',
+      'Redirected to user show page');
+    equal(find('h3:contains("Faizaan Shamsi\'s Profile")').length, 1);
+    // test that a user is logged in, too?
+  });
+});
