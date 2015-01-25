ca02ccec71fd5fd591da03e970db5176fcbdc018
Helen Hood <helen.c.hood@gmail.com>
2015-01-24 21:48:56 -0500

    Create new task record in tasks/new controller
    Test not passing yet - showing one two many tasks on the show page.
    Building out backend to test in development next.

--- frontend/app/controllers/tasks/new.js
+++ frontend/app/controllers/tasks/new.js
@@ -1,21 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // // This does not set model -- still undefined
-  // setupController: function(controller, task) {
-  //   controller.set('model', task);
-  // },
-
   actions: {
     save: function() {
       var _this = this;
-      var model = this.get('model');
-      debugger;
+      var model = this.store.createRecord('task');
 
       model.save().then(function() {
-        debugger;
-        // need to transistion to project show page, not task show page
-        // _this.transitionTo('projects.show', model);
+        // stay on project show page
       }, function() {
         // Failed!
       });
