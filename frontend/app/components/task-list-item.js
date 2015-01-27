eccbab4f88cd826508a9234a932054a059f892c8
Helen Hood <helen.c.hood@gmail.com>
2015-01-27 11:09:08 -0500

    Complete button sets task's completed_at
    Doesn’t yet replace button with “Completed”.

--- frontend/app/components/task-list-item.js
+++ frontend/app/components/task-list-item.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'li',
-  classNames: ['task-list-item']
+  classNames: ['task-list-item'],
+
+  actions: {
+    saveTask: function() {
+      var task = this.get('task');
+      task.set('completedAt', new Date());
+      task.save().then(function() {
+        console.log(task.completedAt);
+      });
+    }
+  }
 });
