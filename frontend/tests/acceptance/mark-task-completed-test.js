2c7a4234dbd51f581a95594bb2c26b8d1de7f7ef
Helen Hood <helen.c.hood@gmail.com>
2015-01-27 11:52:24 -0500

    Only project creator can mark a task complete
    I don’t actually need the session.isAuthenticated conditional in my
    component template.  The canEdit property takes care of that.  I can’t
    get the acceptance test to pass without it, though, unless I mock the
    server response so the project’s canEdit property is false.  Will
    change that next.

--- frontend/tests/acceptance/mark-task-completed-test.js
+++ frontend/tests/acceptance/mark-task-completed-test.js
@@ -7,6 +7,7 @@ var application, server;
 module('Acceptance: MarkTaskCompleted', {
   setup: function() {
     application = startApp();
+
     var project = {
       id: 1,
       name: 'Smite the world with plague of man-eating ladybugs',
@@ -55,9 +56,51 @@ test('project creator marks task complete', function() {
 });
 
 test('other user cannot mark task complete', function() {
-  expect(0);
+  var project = {
+    id: 1,
+    name: 'Smite the world with plague of man-eating ladybugs',
+    description: 'They will never see it coming.',
+    user_id: 3,
+    canEdit: false
+  };
+
+  var user = {
+    id: 3,
+    firstName: 'Faizaan',
+    lastName: 'The Wizard',
+    email: 'faizaan@nefariousschemers.com',
+    bio: 'I am an evil schemer.',
+    project_ids: [1, 2, 3]
+  };
+
+  var tasks = [
+    { id: 1,
+      project_id: 1,
+      name: 'Genetically modify ladybugs'
+    }
+  ];
+
+  server = new Pretender(function(){
+    this.get('/api/v1/projects/:id', function(request) {
+      return [200, {"Content-Type": "application/json"}, JSON.stringify({project: project, users: [user], tasks: tasks})];
+    });
+  });
+
+  authenticateSession();
+
+  visit('/projects/1');
+
+  andThen(function() {
+    equal(find('button:contains("Complete")').length, 0);
+  });
 });
 
 test('unauthenticated user cannot mark task complete', function() {
-  expect(0);
+  invalidateSession();
+
+  visit('/projects/1');
+
+  andThen(function() {
+    equal(find('button:contains("Complete")').length, 0);
+  });
 });
